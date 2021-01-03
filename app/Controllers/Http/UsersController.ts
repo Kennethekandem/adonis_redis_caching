3a59847ce288a7a87b677af764a1c5b92713b0f2
Kenneth Ekandem <kennyekandem@gmail.com>
2021-01-03 10:08:43 +0100

    update

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,4 +1,11 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class UsersController {
+
+    public async index() {
+        
+        const users = await Database.query().select('*').from('users');
+        return users;
+    }
 }
