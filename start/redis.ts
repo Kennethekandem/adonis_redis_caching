4db72fbdd6bf3c1e36ff3c055458bb3313611418
Kenneth Ekandem <kennyekandem@gmail.com>
2021-01-04 12:29:17 +0100

    update

--- start/redis.ts
+++ start/redis.ts
@@ -7,3 +7,12 @@
 | boot.
 |
 */
+
+import Redis from '@ioc:Adonis/Addons/Redis'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+Redis.subscribe('user', () => {
+    const users = Database.query().select('*').from('users');
+
+    Redis.set('orders', JSON.stringify(users));
+})
